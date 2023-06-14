620b517d62e81bdac55ee4c7dde168f9d5e71129
profdl <112704467+profdl@users.noreply.github.com>
2023-06-14 18:43:30 -0400

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="canvas-element">
-    <canvas ref="canvas" id="canvas"></canvas>
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
 
-
 <script>
+// I have no idea what I'm doing
 export default {
   props: {
     content: { type: Object, required: true },
@@ -18,8 +18,28 @@ export default {
     },
   },
   mounted() {
-    this.setupCanvas();
-  },
- 
+    // Create a wrapper around the native canvas element
+    this.canvas = new fabric.Canvas(this.$refs.canvas);
+
+    // Create a rectangle object
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20
+    });
+
+    // Add the rectangle onto the canvas
+    this.canvas.add(rect);
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.my-element {
+  p {
+    font-size: 18px;
+  }
+}
+</style>
