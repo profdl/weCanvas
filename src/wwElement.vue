f18792a6224de14723ad0b81d3176f8f34e9cdc1
profdl <112704467+profdl@users.noreply.github.com>
2023-06-14 19:26:21 -0400

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -5,41 +5,41 @@
 </template>
 
 <script>
-// I have no idea what I'm doing
+import fabric from 'fabric';
+
 export default {
-  props: {
-    content: { type: Object, required: true },
-  },
-  computed: {
-    textStyle() {
-      return {
-        color: this.content.textColor,
-      };
-    },
-  },
   mounted() {
-    // Create a wrapper around the native canvas element
-    this.canvas = new fabric.Canvas(this.$refs.canvas);
+    this.initializeCanvas();
+  },
+  methods: {
+    initializeCanvas() {
+      this.canvas = new fabric.Canvas(this.$refs.canvas);
 
-    // Create a rectangle object
-    const rect = new fabric.Rect({
-      left: 100,
-      top: 100,
-      fill: 'red',
-      width: 20,
-      height: 20
-    });
+      // Create a rectangle object
+      const rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+      });
 
-    // Add the rectangle onto the canvas
-    this.canvas.add(rect);
+      // Add the rectangle onto the canvas
+      this.canvas.add(rect);
+    },
+    addRectangle() {
+      // Create a rectangle object
+      const rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+      });
+
+      // Add the rectangle onto the canvas
+      this.canvas.add(rect);
+    }
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.my-element {
-  p {
-    font-size: 18px;
-  }
-}
-</style>
