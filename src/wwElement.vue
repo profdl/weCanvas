ad6d71309d2f5ecaad09a5949386f4ff0d30a182
profdl <112704467+profdl@users.noreply.github.com>
2023-06-14 15:46:30 -0400

    Initial commit

---
+++ src/wwElement.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="canvas-element">
+    <canvas ref="canvas" id="canvas"></canvas>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    textStyle() {
+      return {
+        color: this.content.textColor,
+      };
+    },
+  },
+  mounted() {
+    this.setupCanvas();
+  },
+
+ 
+};
+</script>
+
+
